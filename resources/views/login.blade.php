b3b796f433307e2cab5e55c81336c8abcc2c5377
Fakhrizal Akbar <222212595@stis.ac.id>
2024-12-25 15:20:41 +0700

    coba dulu gan

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,16 +17,16 @@
             @csrf
             <div>
                 <label for="nip_bps" class="block text-sm font-medium text-gray-600">NIP BPS</label>
-                <input type="text" name="nip_bps" id="nip_bps" placeholder="Masukkan NIP BPS" 
-                       class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none">
+                <input type="text" name="nip_bps" id="nip_bps" placeholder="Masukkan NIP BPS"
+                    class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none">
             </div>
             <div>
                 <label for="password" class="block text-sm font-medium text-gray-600">Password</label>
-                <input type="password" name="password" id="password" placeholder="Masukkan Password" 
-                       class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none">
+                <input type="password" name="password" id="password" placeholder="Masukkan Password"
+                    class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none">
             </div>
-            <button type="submit" 
-                    class="w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 rounded-lg">
+            <button type="submit"
+                class="w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 rounded-lg">
                 Login
             </button>
         </form>
