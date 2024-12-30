eb37c3aa33e883895e4d6f13ff801de10457e381
bagassunu <bagassunu334@gmail.com>
2024-12-30 17:32:46 +0700

    perbaikan error

--- resources/views/penugasan-organik-all.blade.php
+++ resources/views/penugasan-organik-all.blade.php
@@ -11,14 +11,14 @@
     </div>
 
     <div class="w-full flex flex-row justify-between items-center pb-1">
-<<<<<<< HEAD
-        {{-- Search Input --}}
-        <div class="relative flex items-center w-80">
-=======
-        <div class="relative flex items-center w-64">
->>>>>>> 43d0cb70773b25578ec25a80eeb580df0e03d59c
+        <<<<<<< HEAD
+            {{-- Search Input --}}
+            <div class="relative flex items-center w-80">
+            =======
+            <div class="relative flex items-center w-64">
+                >>>>>>> 43d0cb70773b25578ec25a80eeb580df0e03d59c
 
-        </div>
+            </div>
     </div>
 
     {{-- Tabel--}}
@@ -41,11 +41,7 @@
                         <td>{{ $item->nama }}</td>
                         <td class="text-center">
                             <div class="flex justify-center px-2">
-<<<<<<< HEAD
-                                <x-detail-button-table :id="$item->id" :route="''" />
-=======
                                 <a class="px-2 py-1 rounded-md border border-gray-500" href="{{route('detail-organik',['id'=>$item->id])}}">Detail</a>
->>>>>>> 43d0cb70773b25578ec25a80eeb580df0e03d59c
                             </div>
                         </td>
                     </tr>
