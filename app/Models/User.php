8dead456e82a8867ff0d70e9adb1e9f753df3000
bagassunu <bagassunu334@gmail.com>
2025-01-09 07:57:47 +0700

    hahahihi

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,30 +10,19 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'user';
+
     protected $fillable = [
-        'name',
+        'pegawai_id',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
